e1303dd5940837b132da43c860b62d90022ee3c0
Naveen Ramgiri <naveen.ramgiri@gspann.com>
2024-05-15 19:09:14 +0530

    Sticky-navigation Changes

---
+++ blocks/sticky-right-navigation/sticky-right-navigation.js
@@ -0,0 +1,19 @@
+export default function decorate(block) {
+    const rightNavLinks = document.querySelectorAll('h2');
+    const navLinksUl = document.createElement('ul');
+    navLinksUl.classList.add("sticky-ul")
+
+    rightNavLinks.forEach(rightNavLink => {
+        const liElement = document.createElement('li');
+        const anchorElement = document.createElement('a');
+
+        anchorElement.textContent = rightNavLink.textContent;
+        anchorElement.href = `#${rightNavLink.id}`;
+
+        liElement.appendChild(anchorElement);
+        navLinksUl.appendChild(liElement);
+    });
+
+    block.appendChild(navLinksUl);
+}
+
