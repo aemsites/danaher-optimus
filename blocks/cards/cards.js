0ad7f8fb6bd2c818445eb0447f305dd5c844b52c
Shivangi Singh <shivangi.singh@gspann.com>
2024-05-31 13:10:26 +0530

    recentChanges

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,7 +1,8 @@
+import { ul, li, div } from '../../scripts/dom-builder.js';
+
 export default function decorate(block) {
-  const parentDiv = document.createElement('div');
-  const ul = document.createElement('ul');
-  ul.classList.add(...'cards-ul grid grid-cols-3 gap-11 max-[799px]:grid-cols-1'.split(' '));
+  const parentDiv = div({ class: 'cards-container' });
+  const cardsUl = ul({ class: 'cards-ul grid grid-cols-3 gap-11 max-[799px]:grid-cols-1' });
 
   [...block.children].forEach((row) => {
     const pictureTag = row.querySelector('picture');
@@ -13,7 +14,7 @@ export default function decorate(block) {
       cardDescription.classList.add(...'card-description h-full mt-2.5 mb-3 text-base tracking-wide'.split(' '));
     }
 
-    const cardLink = row.querySelector('a');
+    const cardLink = row.querySelector('p a');
     if (cardLink) {
       cardLink.classList.add(...'card-link w-fit text-sm text-white bg-[#2A5F65] hover:bg-[#255159] py-2.5 px-5 rounded-[28px]'.split(' '));
     }
@@ -22,21 +23,25 @@ export default function decorate(block) {
       cardHeading.classList.add(...'text-5xl mb-8 mt-[72px]'.split(' '));
       parentDiv.appendChild(cardHeading);
     } else {
-      const li = document.createElement('li');
-      li.classList.add(...'cards-li flex flex-col bg-[#e5e7eb]'.split(' '));
+      const cardsLi = li({ class: 'cards-li flex flex-col bg-[#e5e7eb]' });
 
-      const cardContentDiv = document.createElement('div');
-      cardContentDiv.classList.add(...'cards-card-body py-9 px-8 flex flex-col grow'.split(' '));
+      const cardContentDiv = div({ class: 'cards-card-body py-9 px-8 flex flex-col grow' });
 
       cardContentDiv.appendChild(cardHeading);
       cardContentDiv.appendChild(cardDescription);
       cardContentDiv.appendChild(cardLink);
 
-      li.appendChild(pictureTag);
-      li.appendChild(cardContentDiv);
-      ul.appendChild(li);
+      const imgTag = pictureTag.querySelector('img');
+      if (imgTag) {
+        imgTag.classList.add('max-[799px]:w-full');
+      }
+
+      cardsLi.appendChild(pictureTag);
+      cardsLi.appendChild(cardContentDiv);
+      cardsUl.appendChild(cardsLi);
     }
   });
-  parentDiv.appendChild(ul);
+
   block.appendChild(parentDiv);
+  parentDiv.appendChild(cardsUl);
 }
--- blocks/sticky-right-navigation/sticky-right-navigation.js
+++ blocks/sticky-right-navigation/sticky-right-navigation.js
@@ -1,11 +1,11 @@
-import { a, li } from '../../scripts/dom-builder.js';
+import { ul, li, a } from '../../scripts/dom-builder.js';
 
 export default function decorate(block) {
   const rightNavLinks = document.querySelectorAll('h2');
-  const navLinksUl = document.createElement('ul');
-  navLinksUl.classList.add(...'sticky mt-0 max-[768px]:hidden'.split(' '));
+  const navLinksUl = ul({ class: 'sticky-ul max-[768px]:hidden' });
   rightNavLinks.forEach((rightNavLink) => {
     const liElement = li({ class: 'w-72 rounded-3xl mb-2 pr-3.5 pl-3.5 py-2.5 hover:bg-[#f2f2f2]' });
+
     const anchorElement = a({ class: 'text-base text-black font-bold' });
 
     anchorElement.textContent = rightNavLink.textContent;
