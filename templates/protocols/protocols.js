4480705b3a1bfda86d31d53aa124a324d66a86ad
venkateshamkeerthi <venkatesham.keerthi@gspann.com>
2024-06-05 14:36:11 +0530

    fixed conflict issues

--- templates/protocols/protocols.js
+++ templates/protocols/protocols.js
@@ -32,8 +32,7 @@ export default function buildAutoBlocks(block) {
   const headTitle = div(
     { class: 'border border-b-slate-400 mb-10' },
     div(
-      { class: 'w-3/4 m-auto my-12 font-sans text-base flex flex-col justify-center' },
-      breadcrumb(),
+      { class: 'w-3/4 m-auto my-12 font-sans text-base flex flex-col justify-center' },breadcrumb(),
       h1({ class: 'my-5 text-black-0 text-6xl font-semibold tracking-normal' }, title),
       div({ class: 'w-1/6 mb-5 border-t-4 border-[#ff7223]' }),
       div({ class: 'text-xl tracking-normal' }, description),
