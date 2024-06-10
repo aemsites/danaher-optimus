1007d4343b32545192db4233ac649b0a3997a9c9
Shivangi Singh <shivangi.singh@gspann.com>
2024-06-10 14:58:53 +0530

    changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -40,18 +40,6 @@ function buildHeroBlock(main) {
   }
 }
 
-// Change to first letter  is Capital in each word
-function capitalizeWords(str) {
-  const words = str.split(' ');
-  const capitalizedWords = words.map((word) => {
-    if (word.length > 0) {
-      return word.charAt(0).toUpperCase() + word.slice(1);
-    }
-    return word;
-  });
-  return capitalizedWords.join(' ');
-}
-
 /**
  * load fonts.css and set a session storage flag
  */
