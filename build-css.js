dd6fdd517ddec2d81980c2a186d71a998c814076
venkateshamkeerthi <166115333+venkateshamkeerthi@users.noreply.github.com>
2024-06-11 17:52:53 +0530

    Update build-css.js

--- build-css.js
+++ build-css.js
@@ -73,6 +73,11 @@ const fileMappings = [
     output: './blocks/alert-banner/alert-banner.css',
     wrapper: 'alert-banner-wrapper',
   },
+  {
+    content: './blocks/breadcrumb/breadcrumb.js',
+    output: './blocks/breadcrumb/breadcrumb.css',
+    wrapper: 'breadcrumb-wrapper',
+  },
 ];
 
 const watch = process.argv[2];
