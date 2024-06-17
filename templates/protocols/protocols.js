1fff510af2ef24a601e1414ecc10700d1be96ebb
rgravitvl <ravitvl83@gmail.com>
2024-06-17 13:35:30 +0530

    updated protocol template

--- templates/protocols/protocols.js
+++ templates/protocols/protocols.js
@@ -4,8 +4,8 @@ export default async function buildAutoBlocks() {
   const container = document.querySelector('main div');
 
   const titleBlock = buildBlock('title-card', { elems: []});
-  titleBlock.classList.add('block');
-  titleBlock.dataset.blockName = 'title-card';
+  // titleBlock.classList.add('block');
+  // titleBlock.dataset.blockName = 'title-card';
 
   container.append(
     titleBlock,
