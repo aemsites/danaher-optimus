74d492e623df6eb30673b108e590526d114fbdf6
rgravitvl <ravitvl83@gmail.com>
2024-06-17 14:37:49 +0530

    fixed lint issues

--- templates/protocols/protocols.js
+++ templates/protocols/protocols.js
@@ -1,15 +1,13 @@
-import { buildBlock } from "../../scripts/aem.js";
-import { div } from "../../scripts/dom-builder.js";
+import { buildBlock } from '../../scripts/aem.js';
+import { div } from '../../scripts/dom-builder.js';
 
 export default async function buildAutoBlocks() {
   const main = document.querySelector('main');
   const section = main.querySelector('div');
-  const headerSection = div({class: 'border border-b-slate-400 mb-10'});
-  const titleBlock = buildBlock('title-card', { elems: []});
-  const breadcrumbBlock = buildBlock('breadcrumb', { elems: []});
+  const headerSection = div({ class: 'border border-b-slate-400 mb-10' });
+  const titleBlock = buildBlock('title-card', { elems: [] });
+  const breadcrumbBlock = buildBlock('breadcrumb', { elems: [] });
 
-  headerSection.append(
-    breadcrumbBlock, titleBlock,
-  )
+  headerSection.append(breadcrumbBlock, titleBlock);
   main.insertBefore(headerSection, section);
 }
