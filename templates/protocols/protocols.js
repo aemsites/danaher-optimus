e45b9eb65af0a886d3dcfc236860abb2821ac687
Shivangi Singh <shivangi.singh@gspann.com>
2024-06-18 20:53:27 +0530

    protocolTemplate Changes

--- templates/protocols/protocols.js
+++ templates/protocols/protocols.js
@@ -3,13 +3,11 @@ import { div } from '../../scripts/dom-builder.js';
 
 export default async function buildAutoBlocks() {
   const main = document.querySelector('main');
-  const sections = main.querySelectorAll('div');
-  sections.forEach((section) => {
-    section.classList.add('w-4/5', 'm-auto', 'mb-8');
-  });
+  const section = main.querySelector('div');
+  section.classList.add('w-4/5', 'm-auto', 'mb-8');
   const headerSection = div({ class: 'border border-b-slate-400 mb-10' });
   const titleBlock = buildBlock('title-card', { elems: [] });
   const breadcrumbBlock = buildBlock('breadcrumb', { elems: [] });
   headerSection.append(breadcrumbBlock, titleBlock);
-  main.prepend(headerSection);
+  main.insertBefore(headerSection, section);
 }
