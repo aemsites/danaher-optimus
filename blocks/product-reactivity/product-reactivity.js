7597c76ceebf98fd5b11f5a335976c426cb6fd6f
Rajesh-GSPANN <rajesh.chithaluri@gspann.com>
2024-08-08 12:29:54 +0530

    163|Reverted the changes in the reactivity data

--- blocks/product-reactivity/product-reactivity.js
+++ blocks/product-reactivity/product-reactivity.js
@@ -155,10 +155,10 @@ export default async function decorate(block) {
       ? response[0].raw.reactivitytabledata : [];
     const tableContent = allApplicationTableData(reactivityJson, reactivityApplication);
     reactivityApplicationWrapper.appendChild(tableContent);
-    //const publicationArray = response[0].raw.publicationsjson
-      //? response[0].raw.publicationsjson.slice(0, 2) : [];
-    //const images = response[0].raw.images ? response[0].raw.images.slice(0, 3) : [];
-    //const pubandimagesection = publicationsAndImageSection(images, publicationArray);
+    const publicationArray = response[0].raw.publicationsjson
+    ? response[0].raw.publicationsjson.slice(0, 2) : [];
+    const images = response[0].raw.images ? response[0].raw.images.slice(0, 3) : [];
+    const pubandimagesection = publicationsAndImageSection(images, publicationArray);
     reactivityApplicationWrapper.appendChild(pubandimagesection);
     block.append(reactivityData);
     block.appendChild(reactivityApplicationWrapper);
